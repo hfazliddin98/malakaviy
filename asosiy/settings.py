572dd2011e50112a6d4f670c2b136fccd2b617fd
hfazliddin98 <hatamovfazliddin505@gmail.com>
2024-09-11 21:50:15 +0500

    11.9.2024

--- asosiy/settings.py
+++ asosiy/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',   
 
     # men qoshgan applar
